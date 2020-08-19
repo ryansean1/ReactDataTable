d6ea483869e290756b3f38e4faae8a4d1bca38a2
ryansean1 <seanryan090@gmail.com>
2020-08-19 11:01:59 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,167 +1,262 @@
+import React, { Component } from "react";
+import "./App.css";
+import Toggle2 from "./components/Toggle2";
+import Rectangle from "./components/Rectangle";
+import D3 from "./components/MapExamples/D3Map";
+import PageButtons from "./components/PageButtons";
+import FilterRectangle from "./components/Rectangle1";
+// import StateForm from "./components/Form";
+// import Formcontrol from "./components/Selector";
+import ActiveProviders from "./components/Tables/ActiveProviders";
+import MarketConcentration from "./components/Tables/MarketConcentration";
 
-import './App.css';
-import React, { Component } from 'react';
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+    this.myRef_Participant = React.createRef();
+    this.myRef_Market = React.createRef();
+    this.myRef_Budget = React.createRef();
+    this.state = {
+      filterVal: [],
+      stateList: [],
+      numberProviders: 10,
 
+      // switchText: "Off"
+    };
+  }
 
-// import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+  makeStateList = (list) => {
+    this.setState({ stateList: list });
+  };
+  updateFilter = (value) => {
+    this.setState({ filterVal: value });
+  };
 
-import MenuItem from '@material-ui/core/MenuItem';
+  scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop);
+  scrollToMyRef1 = () =>
+    window.scrollTo(0, this.myRef_Participant.current.offsetTop);
+  scrollToMyRef2 = () =>
+    window.scrollTo(0, this.myRef_Market.current.offsetTop);
+  scrollToMyRef3 = () =>
+    window.scrollTo(0, this.myRef_Budget.current.offsetTop);
 
+  passValue = () => {
+    console.log(this.props.numberProviders);
+  };
 
-// import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import { useTheme } from '../node_modules/@material-ui/styles';
+  render() {
+    const { filterVal } = this.state;
+    const { stateList } = this.state;
+    // var dataPath = require("./data/in_data.csv");
 
-const { useState } = React,
-      { render } = ReactDOM,
-      rootNode = document.getElementById('root')
-      
-const sampleData = [
-        {id: 0, state: 'apple', participants: 'fruit', utilisation: 'green'},
-        {id: 1, state: 'pear', participants: 'fruit', utilisation: 'green'},
-        {id: 2, state: 'banana', participants: 'fruit', utilisation: 'yellow'},
-        {id: 3, state: 'carrot', participants: 'vegie', utilisation: 'red'},
-        {id: 4, state: 'strawberry', participants: 'berry', utilisation: 'red'}
-      ],
-      sampleColumns = [
-        {id: 0, property: 'state', columnLabel: 'State'},
-        {id: 1, property: 'participants', columnLabel: 'Participants'},
-        {id: 2, property: 'utilisation', columnLabel: 'Utilisation'}
-      ]
-     
-    
+    // function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Rectangle />
+          <top className="App-buttons">
+            <br></br>
+            <div>
+              <PageButtons
+                scrollTo={this.scrollToMyRef}
+                scrollToParticipant={this.scrollToMyRef1}
+                scrollToMarket={this.scrollToMyRef2}
+                scrollToBudget={this.scrollToMyRef3}
+                passValue={this.passValue}
+              />
+            </div>
+          </top>
 
+          <br></br>
+          <main className="App-main">
+            <body className="App-mainheading">
+              {/* ---------Active Providers -----------------------------------------------------------------------*/}
+              <div style={{ textAlign: "left" }}>Active Providers</div>
+            </body>
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>Select a Category</div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                Choose single or multiple categories to filter the <br></br>{" "}
+                data displayed on the Active providers section
+              </div>
+              <br></br>
 
-      
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(10),
-      width: '50ch',
-    },
-  },
-}));
+              <form classname="App-form">
+                <ActiveProviders />
+              </form>
+              <br></br>
 
+              {/* <form className="App-form">{<StateForm />}</form> */}
+            </body>
+            <br></br>
 
+            <br></br>
+            <div></div>
 
-function MultilineSelectFields() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [currency, setCurrency] = React.useState('EUR');
-  const [participantsName, setParticipantsName] = React.useState([]);
-  const handleChange = (event) => {
-  setCurrency(event.target.value);
-};
+            {/* ---------Participant Data -----------------------------------------------------------------------*/}
+            <body className="App-mainheading">
+              <div ref={this.myRef_Participant} style={{ textAlign: "left" }}>
+                Participant Data
+              </div>
+            </body>
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>Select a Category</div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                Choose single or multiple categories to filter the <br></br>{" "}
+                data displayed on the Active providers section
+              </div>
+            </body>
+            <br></br>
+            <FilterRectangle />
+            <br></br>
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>
+                Service District Locations
+              </div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                The map below shows the variation in the number of participants
+                across <br></br>
+                Service Districts; lighter colours correspond to lower numbers
+                and<br></br>
+                darker colours corresond to higher numbers based on category
+                <br></br>
+                selections. Service District boundaries are based on 2011 Local{" "}
+                <br></br>
+                Government Areas (LGAs).
+              </div>
+            </body>
+            <br></br>
+            <D3 />
+            <br></br>
 
-const handleChange1 = (event) => {
-  setParticipantsName(event.target.value);
-};
+            {/* ---------Market Concentration -----------------------------------------------------------------------*/}
+            <body className="App-mainheading">
+              <div ref={this.myRef_Market} style={{ textAlign: "left" }}>
+                Market Concentration
+              </div>
+            </body>
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>Select a Category</div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                Choose single or multiple categories to filter the <br></br>{" "}
+                data displayed on the Active providers section
+              </div>
+            </body>
+            <FilterRectangle />
+            <br></br>
+            <MarketConcentration />
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>
+                Service District Locations
+              </div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                The map below shows the variation in the number of participants
+                across <br></br>
+                Service Districts; lighter colours correspond to lower numbers
+                and<br></br>
+                darker colours corresond to higher numbers based on category
+                <br></br>
+                selections. Service District boundaries are based on 2011 Local{" "}
+                <br></br>
+                Government Areas (LGAs).
+              </div>
+            </body>
+            <br></br>
+            <body className="App-body">
+              {" "}
+              <Toggle2 />{" "}
+            </body>
 
+            <br></br>
+            {/* -----
+            {/* ---------Utilisation of Plan Budgets -----------------------------------------------------------------------*/}
+            <body className="App-mainheading">
+              <div ref={this.myRef_Budget} style={{ textAlign: "left" }}>
+                Utilisation of Plan Budgets
+              </div>
+            </body>
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>Select a Category</div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                Choose single or multiple categories to filter the <br></br>{" "}
+                data displayed on the Active providers section
+              </div>
+            </body>
+            <br></br>
+            <FilterRectangle />
+            <br></br>
+            <body className="App-subheading">
+              <div style={{ textAlign: "left" }}>
+                Service District Locations
+              </div>
+            </body>
+            <body className="App-body">
+              <div style={{ textAlign: "left" }}>
+                The map below shows the variation in the number of participants
+                across <br></br>
+                Service Districts; lighter colours correspond to lower numbers
+                and<br></br>
+                darker colours corresond to higher numbers based on category
+                <br></br>
+                selections. Service District boundaries are based on 2011 Local{" "}
+                <br></br>
+                Government Areas (LGAs).
+              </div>
+            </body>
+            <br></br>
+            <D3 />
+            <br></br>
+            {/* -----
+
+            {/* <Toggle2
+              style={{ textAlign: "left" }}
+              myData={stateList}
+              filterVal={filterVal}
+            /> */}
+            {/* <br></br>
+            <h3 style={{ color: "Red", textAlign: "left" }}>
+              Filterable Table - Press CTRL and Click to clear filter -{" "}
+              <br></br>select state from dropdown
+            </h3>
+            <div>
+              {filterVal.map((txt) => (
+                <>{txt} </>
+              ))}
+            </div>
+            <StateSelect
+              name="filterVal"
+              stateList={stateList}
+              selectionUpdate={(value) => this.updateFilter(value)}
+            /> */}
+            {/* <StateSelect stateList={stateList} selectionUpdate={(value) => this.updateVar(filterVal, value)} /> */}
+            {/* <FilterTable myData={stateList} filterVal={filterVal} />
+            <DataOnly
+              csvFilePath={dataPath}
+              myGetData={(value) => this.makeStateList(value)}
+            /> */}
+            {/* <StateSelect /> */}
+            {/* <h3 style={{ color: "Red", textAlign: "left" }}>
+              Number being displayed inside Circle - <br></br>
+            </h3> */}
+            {/* <NumberSwitch /> */}
+          </main>
+        </header>
+      </div>
+    );
+  }
 }
-
-      
-const MyFilter = ({filterProperties, onFilter}) => (
-  <Container>
-    {
-      filterProperties.map(({participantsName,id}) => (
-        <TextField
-           id="standard-select-state"
-            select
-            label="test"
-            value={participantsName}
-            onChange={MultilineSelectFields.handleChange1}
-         
-            
-          />
-        
-        
-      ))
-    }
-  </Container>
-)
-
-const MyTable = ({tableData, tableColumns}) => (
-  <TableContainer>
-    <Table>
-      <TableHead>
-        <TableRow>
-          {
-            tableColumns.map(({id, columnLabel}) => (
-              <TableCell key={id}>
-                {columnLabel}
-              </TableCell>
-            ))
-          }
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {
-          tableData.map(row => (
-            <TableRow key={row.id}>
-              {
-                tableColumns.map(({id, property}) => (
-                  <TableCell key={id}>
-                    {row[property]}
-                  </TableCell>
-                ))
-              }
-            </TableRow>
-          ))
-        }
-      </TableBody>
-    </Table>
-  </TableContainer>
-)
-
-const App = () => {
-  const [state, setState] = useState({
-          data: sampleData,
-          columns: sampleColumns,
-          filterObj: sampleColumns.reduce((r,{property}) => (r[property]='', r), {})
-        }),
-        onFilterApply = ({target:{state,value}}) => {
-          const newFilterObj = {...state.filterObj, [state]: value}
-          setState({
-            ...state,
-            filterObj: newFilterObj,
-            data: sampleData.filter(props => 
-              Object
-                .entries(newFilterObj)
-                .every(([key,val]) => 
-                  !val.length ||
-                  props[key].toLowerCase().includes(val.toLowerCase()))
-            )
-          })
-        }
-  return (
-    <Container>
-      <MyFilter 
-        filterProperties={state.columns}
-        onFilter={onFilterApply}
-      />
-      <MyTable
-        tableData={state.data}
-        tableColumns={state.columns}
-      />
-      {/* <FilterForm/> */}
-     {/* < MultilineTextFields/> */}
-    </Container>
-  )
-}
-
 
 export default App;
